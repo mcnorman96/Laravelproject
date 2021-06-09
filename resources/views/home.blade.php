3a68ad19dde89c458a0c6c9d373bf95db21325cc
Norman <marcus-norman@hotmail.com>
2021-06-09 22:17:10 +0200

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <div class="row">
+    <div class="col-3">
+        <h1>yeet</h1>
+    </div>
+    <div class="col9">
+
+    </div>
+   </div>
+
+   <div class="row pt-5">
+    <div class="col-4">
+        <img style="width: 100%" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" alt="">
+    </div>
+    <div class="col-4">
+        <img style="width: 100%" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" alt="">
+    </div>
+    <div class="col-4">
+        <img style="width: 100%" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" alt="">
+    </div>
+   </div>
+</div>
+@endsection
