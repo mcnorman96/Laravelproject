9f759da800c53f5d4c9c5db51397c9e5fadcc84d
Norman <marcus-norman@hotmail.com>
2021-06-09 22:52:20 +0200

    init

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
     </div>
    </div>
 
-   <div class="row pt-5">
+   <div class="row pt-5 ">
     <div class="col-4">
         <img style="width: 100%" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" alt="">
     </div>
